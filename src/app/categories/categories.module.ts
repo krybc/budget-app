61229908a103d78330139096e1ec17ce9cc1fe08
Kamil Rybczynski <github@rybczynski.io>
2021-03-06 21:11:12 +0100

    - imported MatButtonModule to CategoriesModule

--- src/app/categories/categories.module.ts
+++ src/app/categories/categories.module.ts
@@ -1,24 +1,27 @@
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
-
-import {ShellComponent} from '@categories/shell.component';
-
-import {CategoriesRoutingModule} from './categories-routing.module';
-import {CategoryAddComponent} from './containers/category-add/category-add.component';
-import {CategoryEditComponent} from './containers/category-edit/category-edit.component';
-import { CategoryFormComponent } from './components/category-form/category-form.component';
+import {MatButtonModule} from '@angular/material/button';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSelectModule} from '@angular/material/select';
 import {MatCardModule} from '@angular/material/card';
+
 import {SharedUiModule} from '@shared/ui';
 import {SharedSpinnerModule} from '@shared/spinner';
 
+import {CategoriesRoutingModule} from './categories-routing.module';
+import {CategoryAddComponent} from './containers/category-add/category-add.component';
+import {CategoryEditComponent} from './containers/category-edit/category-edit.component';
+import { CategoryFormComponent } from './components/category-form/category-form.component';
+import {ShellComponent} from './shell.component';
+
+
 const uiModules = [
   FlexLayoutModule,
+  MatButtonModule,
   MatCardModule,
   MatFormFieldModule,
   MatInputModule,
